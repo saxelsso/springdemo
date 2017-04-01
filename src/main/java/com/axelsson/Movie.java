e7d10ffbe8952538e8c31737b8ea33a8f16ca0b3
Stefan Axelsson <stefan@axelsson.com>
2017-04-01 16:42:05 +0200

    JSON implementation

---
+++ src/main/java/com/axelsson/Movie.java
@@ -0,0 +1,24 @@
+package com.axelsson;
+
+public class Movie {
+
+	public long id;
+	public String name;
+	
+	public Movie(long id, String name) {;
+		this.id = id;
+		this.name = name;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
+		
+	
+	
--- src/main/java/com/axelsson/MoviesController.java
+++ src/main/java/com/axelsson/MoviesController.java
@@ -3,6 +3,9 @@ package com.axelsson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.*;
 
 // template processing
 import org.springframework.ui.Model;
@@ -10,13 +13,37 @@ import org.springframework.ui.Model;
 @Controller
 public class MoviesController {
     
+    @RequestMapping("/movies_json")
+    public @ResponseBody List index_json(@RequestParam(value="name", required=false, defaultValue="empty") String result) {
+        
+	List myList = new ArrayList();
+	
+	Movie myMovie = new Movie (1, "Matrix");
+	myList.add(myMovie);
+	
+	Movie myMovie2 = new Movie (2, "Batman");
+	myList.add(myMovie2);
+	
+	// populate model 
+	//model.addAttribute("listofmovies", result);
+        
+        // dispatch to template
+        return myList;
+    }
+    
     @RequestMapping("/movies")
-    public String index(@RequestParam(value="name", required=false, defaultValue="") String result, Model model) {
+    public String index(@RequestParam(value="name", required=false, defaultValue="empty") String result, Model model) {
         
-        result = "Not implemented yet!";
-        
-        // populate model 
-	model.addAttribute("listofmovies", result);
+	List myList = new ArrayList();
+	
+	Movie myMovie = new Movie (1, "Matrix");
+	myList.add(myMovie);
+	
+	Movie myMovie2 = new Movie (2, "Batman");
+	myList.add(myMovie2);
+	
+	// populate model 
+	model.addAttribute("listofmovies", myList);
         
         // dispatch to template
         return "movies";
