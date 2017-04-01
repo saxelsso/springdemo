54d564ed75e684ee1ac789e9ed31b229832661ae
saxelsso <stefan@axelsson.com>
2017-04-01 17:10:26 +0200

    Update MoviesController.java
    cleaned up the code

--- src/main/java/com/axelsson/MoviesController.java
+++ src/main/java/com/axelsson/MoviesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.*;
 
-// template processing
+// HTML template processing
 import org.springframework.ui.Model;
 
 @Controller
@@ -17,35 +17,27 @@ public class MoviesController {
     public @ResponseBody List index_json(@RequestParam(value="name", required=false, defaultValue="empty") String result) {
         
 	List myList = new ArrayList();
-	
 	Movie myMovie = new Movie (1, "Matrix");
-	myList.add(myMovie);
-	
+	myList.add(myMovie);	
 	Movie myMovie2 = new Movie (2, "Batman");
 	myList.add(myMovie2);
-	
-	// populate model 
-	//model.addAttribute("listofmovies", result);
-        
-        // dispatch to template
+   
+        // dispatch directly as JSON, no HTML template needed
         return myList;
     }
     
     @RequestMapping("/movies")
     public String index(@RequestParam(value="name", required=false, defaultValue="empty") String result, Model model) {
         
-	List myList = new ArrayList();
-	
+	List myList = new ArrayList();	
 	Movie myMovie = new Movie (1, "Matrix");
-	myList.add(myMovie);
-	
+	myList.add(myMovie);	
 	Movie myMovie2 = new Movie (2, "Batman");
 	myList.add(myMovie2);
 	
-	// populate model 
-	model.addAttribute("listofmovies", myList);
-        
-        // dispatch to template
+	// populate model with myList
+	model.addAttribute("listofmovies", myList);        
+        // dispatch to HTML template
         return "movies";
     }
     
