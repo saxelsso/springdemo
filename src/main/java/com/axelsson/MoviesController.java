913791433986f891557cb0eec08a7540c72dcbbe
Stefan Axelsson <stefan@axelsson.com>
2017-04-01 19:53:11 +0200

    general cleanup

--- src/main/java/com/axelsson/MoviesController.java
+++ src/main/java/com/axelsson/MoviesController.java
@@ -13,24 +13,6 @@ import org.springframework.ui.Model;
 @Controller
 public class MoviesController {
     
-    @RequestMapping("/movies_json")
-    public @ResponseBody List index_json(@RequestParam(value="name", required=false, defaultValue="empty") String result) {
-        
-	List myList = new ArrayList();
-	
-	Movie myMovie = new Movie (1, "Matrix");
-	myList.add(myMovie);
-	
-	Movie myMovie2 = new Movie (2, "Batman");
-	myList.add(myMovie2);
-	
-	// populate model 
-	//model.addAttribute("listofmovies", result);
-        
-        // dispatch to template
-        return myList;
-    }
-    
     @RequestMapping("/movies")
     public String index(@RequestParam(value="name", required=false, defaultValue="empty") String result, Model model) {
         
@@ -42,11 +24,27 @@ public class MoviesController {
 	Movie myMovie2 = new Movie (2, "Batman");
 	myList.add(myMovie2);
 	
-	// populate model 
+	// prepare model object for HTML template
 	model.addAttribute("listofmovies", myList);
         
-        // dispatch to template
+        // dispatch via HTML template
         return "movies";
     }
+    
+    @RequestMapping("/movies_json")
+    public @ResponseBody List index_json(@RequestParam(value="name", required=false, defaultValue="empty") String result) {
+        
+	List myList = new ArrayList();
+	
+	Movie myMovie = new Movie (1, "Matrix");
+	myList.add(myMovie);
+	
+	Movie myMovie2 = new Movie (2, "Batman");
+	myList.add(myMovie2);
+        
+        // dispatch directly as JSON
+        return myList;
+    }
+
     
 }
