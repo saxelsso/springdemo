6a3cf89c25284e927c332b1bf6a8475431101f8a
Stefan Axelsson <stefan@axelsson.com>
2017-04-08 23:05:11 +0200

    test of remove

---
+++ src/main/java/com/axelsson/Moviemaker.java
@@ -0,0 +1,24 @@
+package com.axelsson;
+
+public class Moviemaker {
+
+	public String id;
+	public String name;
+	
+	public Actor(String id, String name) {;
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
+		
+	
+	
